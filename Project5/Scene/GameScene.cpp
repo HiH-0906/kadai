f28e3783ea2660f55a0a78f0451593cbd1407789
HiH-0906 <homuihomu0906@gmail.com>
2019-11-05 19:57:34 +0900

    11

--- Project5/Scene/GameScene.cpp
+++ Project5/Scene/GameScene.cpp
@@ -23,8 +23,6 @@ GameScene::GameScene()
 	_objList.emplace_back(
 		new Player({ 100,400 }, { 0,0 })
 	);
-	Vector2 tmpScreen = lpSceneMng.ScreenSize;
-	tmpScreen.y = (tmpScreen.y - 30) / 2;
 	for (int y = 0; y < 5; y++)
 	{
 		for (int x = 0; x < 10; x++)
@@ -37,13 +35,13 @@ GameScene::GameScene()
 			tmpEnemyState.emplace_back(MOVE_TYPE::PITIN, Vector2Dbl{ (35.0 * 7) + (35.0*x), 40.0 + ((40.0)*y) });
 			tmpEnemyState.emplace_back(MOVE_TYPE::LR, Vector2Dbl{ 180.0,0.0 });
 			cnt=cnt % 6;
-			EnemyState state = { static_cast<ENEMY_TYPE>(rand() % static_cast<int>(ENEMY_TYPE::MAX)),							// ﾀｲﾌﾟの設定
-								{ static_cast<double>((tmpScreen.x*(cnt % 2) - 15) + (32 * (cnt % 2))),						// 座標Xの設定
-								static_cast<double>(tmpScreen.y*((cnt / 2) % 3) - 16)},										// 座標Yの設定
-								{ 30.0,32.0 },																					// ｻｲｽﾞの設定
-								std::atan2(40.0 + ((40.0)*y - (tmpScreen.y*((cnt / 2) % 3) - 16)),(35.0 * 7) +
-								(35.0*x) - (tmpScreen.x*(cnt % 2) - 15) + (32 * (cnt % 2))),
-								tmpEnemyState																					// 行動管理の設定
+			EnemyState state = { static_cast<ENEMY_TYPE>(rand() % static_cast<int>(ENEMY_TYPE::MAX)),													// ﾀｲﾌﾟの設定
+								{ static_cast<double>((lpSceneMng.ScreenSize.x*(cnt % 2) - 15) + (32 * (cnt % 2))),										// 座標Xの設定
+								static_cast<double>(((lpSceneMng.ScreenSize.y-30)/2)*((cnt / 2) % 3) - 16)},											// 座標Yの設定
+								{ 30.0,32.0 },																											// ｻｲｽﾞの設定
+								std::atan2(40.0 + ((40.0)*y - (((lpSceneMng.ScreenSize.y - 30) / 2)*((cnt / 2) % 3) - 16)),(35.0 * 7) +					// 角度
+								(35.0*x) - (lpSceneMng.ScreenSize.x*(cnt % 2) - 15) + (32 * (cnt % 2))) + (3.14159 / 2),
+								tmpEnemyState																											// 行動管理の設定
 								};
 			_objList.emplace_back(
 				new Enemy(state)
